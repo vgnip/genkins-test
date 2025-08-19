dd870274fb15454c594efb147f874e743c1ce1d4
Dreamy-dengpong <2450323563@qq.com>
2025-08-20 00:16:00 +0800

    feat: 测试项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { onMounted } from "vue";
+import { autoRefresh } from "./utils/auto-update";
+
+// import * as pdfLib from "pdf-dist";
+
+// console.log("pdfLib", pdfLib);
+// onMounted(() => {
+//   if (import.meta.env.MODE == "development") {
+//     autoRefresh();
+//   }
+// });
+</script>
+
+<template></template>
